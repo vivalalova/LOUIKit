2768ceab8522ac85b5099a67cd0bde574a98dcb0
vivalalova <vivalalova0@gmail.com>
2016-04-07 19:41:04 +0800

    15

--- Pod/Classes/LOTableView.m
+++ Pod/Classes/LOTableView.m
@@ -62,9 +62,6 @@
     }
 }
 
-- (void)dealloc {
-    [self removeObserver:self forKeyPath:kContentOffset];
-}
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
     if ([keyPath isEqualToString:kContentOffset]) {
