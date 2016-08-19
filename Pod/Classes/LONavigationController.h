9326a8e56d0e36c330cbf5facfed5b1b93eb63de
vivalalova <vivalalova0@gmail.com>
2016-08-20 03:32:24 +0800

    (╬ ಠ 益ಠ)

--- Pod/Classes/LONavigationController.h
+++ Pod/Classes/LONavigationController.h
@@ -9,6 +9,9 @@
 #import <UIKit/UIKit.h>
 IB_DESIGNABLE
 @interface LONavigationController : UINavigationController
+/**
+ *  Set UIViewControllerBasedStatusBarAppearance to YES in info.plist
+ */
 @property (assign,nonatomic) IBInspectable BOOL lightStatusBar;
 
 @end
