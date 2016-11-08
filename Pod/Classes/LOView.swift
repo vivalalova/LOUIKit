3233ab47662e317154c181db3a8de4bb4640e6ee
vivalalova <vivalalova0@gmail.com>
2016-11-08 16:30:02 +0800

    from protocol

--- Pod/Classes/LOView.swift
+++ Pod/Classes/LOView.swift
@@ -6,108 +6,62 @@
 //
 //
 
-
-
-
 import UIKit
 
-@IBDesignable public class LOView: UIView {
-    
-    public required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-    
-    open override func prepareForInterfaceBuilder() {
-        super.prepareForInterfaceBuilder()
-        self.setup()
-    }
-    
-    open override func layoutSubviews() {
-        super.layoutSubviews()
-        self.setShadow()
-    }
-    
-    func setup() {
-        self.setShadow()
-    }
-    
-    func setShadow() {
-        self.layer.shadowRadius = self.shadowRadius
-        self.layer.shadowOffset = self.shadowOffset!
-        self.layer.shadowOpacity = self.shadowOpacity!
-        self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.shadowRadius).cgPath
-    }
-}
-extension LOView:LOViewProtocol {
-    @IBInspectable public var shadowRadius:CGFloat {
+extension UIView:LOViewIBInspectable {
+
+    @IBInspectable var borderColor:UIColor {
         get{
-            return self.getShadowRadius()
-        }
-        set {
-            self.setShadowRadius(newValue: newValue)
-        }
-    }
-    
-    @IBInspectable var shadowOpacity:Float?{
-        get{
-            return self.layer.shadowOpacity
+            let cgColor = self.layer.borderColor ?? UIColor.clear.cgColor
+            return UIColor(cgColor: cgColor)
         }
         set{
-            self.layer.shadowOpacity = newValue ?? 0
+            self.layer.borderColor = newValue.cgColor
         }
     }
-    
-    @IBInspectable var shadowOffset:CGSize?{
-        get{
-            return self.layer.shadowOffset
-        }
-        set{
-            self.layer.shadowOffset = newValue ?? CGSize(width: 0, height: 0)
-        }
-    }
-    
-    @IBInspectable var circle:Bool?{
-        get{
-            return self.layer.cornerRadius == self.frame.size.height / 2
-        }
-        set{
-            circle = newValue
-            if circle == true {
-                self.layer.cornerRadius = self.frame.size.height / 2
-            }
-        }
-    }
-    
-    @IBInspectable var cornerRadius:CGFloat?{
-        get{
-            return self.layer.cornerRadius
-        }
-        set{
-            if circle != true {
-                self.layer.cornerRadius = newValue ?? 0
-            }
-        }
-    }
-    
-    @IBInspectable var borderColor:UIColor?{
-        get{
-            return UIColor(cgColor:self.layer.borderColor!)
-        }
-        set{
-            self.layer.borderColor = newValue?.cgColor ?? UIColor.clear.cgColor
-        }
-    }
-    
-    @IBInspectable var borderWidth:CGFloat?{
+
+    @IBInspectable var borderWidth:CGFloat {
         get{
             return self.layer.borderWidth
         }
         set{
-            self.layer.borderWidth = newValue ?? 0
+            self.layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var shadowRadius:CGFloat {
+        get{
+            return self.layer.shadowRadius
+        }
+        set{
+            self.layer.shadowRadius = newValue
+        }
+    }
+
+    @IBInspectable var shadowOpacity:Float {
+        get{
+            return self.layer.shadowOpacity
+        }
+        set{
+            self.layer.shadowOpacity = newValue
+        }
+    }
+
+    @IBInspectable var shadowOffset:CGSize {
+        get{
+            return self.layer.shadowOffset
+        }
+        set{
+            self.layer.shadowOffset = newValue
+        }
+    }
+
+    @IBInspectable var cornerRadius:CGFloat {
+        get{
+            return self.layer.cornerRadius
+        }
+        set{
+            self.layer.cornerRadius = newValue
         }
     }
 }
--- Pod/Classes/LOViewProtocol.swift
+++ Pod/Classes/LOViewProtocol.swift
@@ -6,24 +6,11 @@
 //
 //
 
-public protocol LOViewProtocol {
-    var shadowRadius: CGFloat { get set }
-//    var shadowOpacity:Float {get set}
-//    var shadowOffset:CGSize {get set}
-//    var circle:Bool {get set}
-//    var cornerRadius:CGFloat {get set}
-//    var borderColor:UIColor {get set}
-//    var borderWidth:CGFloat {get set}
-//    var blurStyle:NSInteger? {get set}
-//    var blur:Bool? {get set}
-}
-
-public extension LOViewProtocol where Self: UIView {
-  
-    public func getShadowRadius() -> CGFloat {
-        return self.layer.shadowRadius
-    }
-    public func setShadowRadius(newValue:CGFloat) {
-        self.layer.cornerRadius = self.shadowRadius
-    }
+protocol LOViewIBInspectable {
+    var borderColor:UIColor {get set}
+    var borderWidth:CGFloat {get set}
+    var shadowRadius:CGFloat {get set}
+    var shadowOpacity:Float {get set}
+    var shadowOffset:CGSize {get set}
+    var cornerRadius:CGFloat {get set}
 }
